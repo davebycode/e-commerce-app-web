dc983946024b6bb394c12ba07767e789d1a49a03
Davidbycode <davidokoyeh@gmail.com>
2023-03-02 16:48:08 +0100

    e-commerce webpage

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+       <a href="/">D Frankey</a> 
+      </header>
+      <main>
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
